6bd1f692db53bca6d08a07f282184c0f21e5d659
kia57085 <kia57085@gmail.com>
2019-09-28 10:24:51 +0300

    Add files via upload

---
+++ SeaBattle/SeaBattle/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SeaBattle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox23 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox23)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox23);
+            this.panel1.Controls.Add(this.pictureBox22);
+            this.panel1.Controls.Add(this.pictureBox21);
+            this.panel1.Controls.Add(this.pictureBox13);
+            this.panel1.Controls.Add(this.pictureBox12);
+            this.panel1.Controls.Add(this.pictureBox11);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(40, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // pictureBox22
+            // 
+            this.pictureBox22.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox22.Location = new System.Drawing.Point(30, 60);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox22.TabIndex = 21;
+            this.pictureBox22.TabStop = false;
+            this.pictureBox22.Click += new System.EventHandler(this.PictureBox22_Click);
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox21.Location = new System.Drawing.Point(0, 60);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox21.TabIndex = 20;
+            this.pictureBox21.TabStop = false;
+            this.pictureBox21.Click += new System.EventHandler(this.PictureBox21_Click);
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox13.Location = new System.Drawing.Point(60, 30);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox13.TabIndex = 12;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Click += new System.EventHandler(this.PictureBox13_Click);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox12.Location = new System.Drawing.Point(30, 30);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox12.TabIndex = 11;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.PictureBox12_Click);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox11.Location = new System.Drawing.Point(0, 30);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox11.TabIndex = 10;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.PictureBox11_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox3.Location = new System.Drawing.Point(60, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.PictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.Location = new System.Drawing.Point(30, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox23
+            // 
+            this.pictureBox23.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox23.Location = new System.Drawing.Point(60, 60);
+            this.pictureBox23.Name = "pictureBox23";
+            this.pictureBox23.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox23.TabIndex = 22;
+            this.pictureBox23.TabStop = false;
+            this.pictureBox23.Click += new System.EventHandler(this.PictureBox23_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 511);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox23)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox22;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox23;
+    }
+}
+
---
+++ SeaBattle/SeaBattle/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeaBattle
+{
+    public partial class Form1 : Form
+    {
+        dynamic miss = Properties.Resources.ground01;
+        dynamic sea = Properties.Resources.ground0;
+        dynamic shooted = Properties.Resources.ground1;
+        dynamic ship = Properties.Resources.ground2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = sea;
+            pictureBox2.Image = sea;
+            pictureBox3.Image = sea;
+
+            pictureBox11.Image = sea;
+            pictureBox12.Image = sea;
+            pictureBox13.Image = sea;
+
+            pictureBox21.Image = sea;
+            pictureBox22.Image = sea;
+            pictureBox23.Image = sea;
+        }
+
+        int[,] battleGround = new int[10, 10];
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+            --battleGround[0, 0];
+            int val = battleGround[0, 0];
+
+            if(val == -1)
+            {
+                pictureBox1.Image = miss;
+            }
+            else if(val == 1)
+            {
+                pictureBox1.Image = shooted;
+            }
+            else if(val == 2)
+            {
+                pictureBox1.Image = (ship);
+            }
+        }
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            --battleGround[0, 1];
+            int val = battleGround[0, 1];
+
+            if (val == -1)
+            {
+                pictureBox2.Image = miss;
+            }
+            else if (val == 1)
+            {
+                pictureBox2.Image = shooted;
+            }
+            else if (val == 2)
+            {
+                pictureBox2.Image = (ship);
+            }
+        }
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+            --battleGround[0, 2];
+            int val = battleGround[0, 2];
+
+            if (val == -1)
+            {
+                pictureBox3.Image = miss;
+            }
+            else if (val == 1)
+            {
+                pictureBox3.Image = shooted;
+            }
+            else if (val == 2)
+            {
+                pictureBox3.Image = ship;
+            }
+        }
+        private void PictureBox11_Click(object sender, EventArgs e)
+        {
+            --battleGround[1, 0];
+            int val = battleGround[1, 0];
+
+            if (val == -1)
+            {
+                pictureBox11.Image = miss;
+            }
+            else if (val == 1)
+            {
+                pictureBox11.Image = shooted;
+            }
+            else if (val == 2)
+            {
+                pictureBox11.Image = ship;
+            }
+        }
+        
+        private void PictureBox12_Click(object sender, EventArgs e)
+        {
+            --battleGround[1, 1];
+            int val = battleGround[1, 1];
+
+            if (val == -1)
+            {
+                pictureBox12.Image = miss;
+            }
+            else if (val == 1)
+            {
+                pictureBox12.Image = shooted;
+            }
+            else if (val == 2)
+            {
+                pictureBox12.Image = ship;
+            }
+        }
+        private void PictureBox13_Click(object sender, EventArgs e)
+        {
+            --battleGround[1, 2];
+            int val = battleGround[1, 2];
+
+            if (val == -1)
+            {
+                pictureBox13.Image = miss;
+            }
+            else if (val == 1)
+            {
+                pictureBox13.Image = shooted;
+            }
+            else if (val == 2)
+            {
+                pictureBox13.Image = ship;
+            }
+        }
+        private void PictureBox21_Click(object sender, EventArgs e)
+        {
+            --battleGround[2, 0];
+            int val = battleGround[2, 0];
+
+            if (val == -1)
+            {
+                pictureBox21.Image = miss;
+            }
+            else if (val == 1)
+            {
+                pictureBox21.Image = shooted;
+            }
+            else if (val == 2)
+            {
+                pictureBox21.Image = ship;
+            }
+        }
+        private void PictureBox22_Click(object sender, EventArgs e)
+        {
+            --battleGround[2, 1];
+            int val = battleGround[2, 1];
+
+            if (val == -1)
+            {
+                pictureBox22.Image = miss;
+            }
+            else if (val == 1)
+            {
+                pictureBox22.Image = shooted;
+            }
+            else if (val == 2)
+            {
+                pictureBox22.Image = ship;
+            }
+        }
+
+        private void PictureBox23_Click(object sender, EventArgs e)
+        {
+            --battleGround[2, 2];
+            int val = battleGround[2, 2];
+
+            if (val == -1)
+            {
+                pictureBox23.Image = miss;
+            }
+            else if (val == 1)
+            {
+                pictureBox23.Image = shooted;
+            }
+            else if (val == 2)
+            {
+                pictureBox23.Image = ship;
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i != 10; ++i)
+            {
+                for (int j = 0; j != 10; ++j)
+                {
+                    battleGround[i, j] = 2;
+                }
+            }
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+    }
+}
